d604ccbdf7e8402219cbaf7fe9b43e3b3f133729
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-11-06 18:19:07 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employeelistcomponent/employeelistcomponent.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: '', redirectTo: 'employees', pathMatch: 'full' },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'add', component:  RegisterComponent },
- 
+  { path: 'profileAdd', component: ReactiveFormComponent },
 ];
 
 @NgModule({
