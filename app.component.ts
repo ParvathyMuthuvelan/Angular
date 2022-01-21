99e27dc4f414c938dcb31d8807a8f5a69574b838
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2022-01-21 17:35:30 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -5,79 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'My first angular ';
-  name="Sai";
+  title = 'AngularProject';
+  name= "Sai";
   clickCount=0;
-  text = ''; 
-  isDisabled=true;
-  isEnabled=false;
+  
+  isDisabled:boolean=true;
+  isEnabled:boolean=false;
   cssStringVar: string= 'red size20';
-  hasError:boolean =false;
-  cssArray:string[]=['red','size20']; 
-  num:number=50;
-  showMe: boolean=false;
-  todaydate = new Date();
-   jsonval = {name:'Rox', age:'25', address:{a1:'Mumbai', a2:'Karnataka'}};
-   months = ["Jan", "Feb", "Mar", "April", "May", "Jun",
-             "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-  items= [
-    new item(1, 'Mobile'),
-    new item(2, 'Laptop'),
-    new item(3, 'Desktop'),
-    new item(4, 'Printer')
-  ]
-  getTitle(): string {
-    return this.title;
-}
+  hasError:boolean =true;
+  cssArray:string[]=['green','size20']; 
 
-max(first: number, second: number): number {
-  return Math.max(first, second);
-}
+  getTitle():string
+  {
+    return this.title
+  }
 
-getClass() {
-  return 'green';
-}
-isError()
-{
-  return true;
-}
-//Event binding
+  getClass() {
+    return 'green';
+  }
+  isError()
+  {
+    return true;
+  }
+
   clickMe() {
     this.clickCount++;
-  }
-  //click event - textbox
-  show(event:any) {
-    console.log("Event target="+event.target);
-    console.log(event.target.value);
-  }    
-  
-    //keyup event
-    displayText(x:any) { 
-      console.log(x.target.value);
-    //this.text=this.text+x.target.value;
-        this.text += x.target.value + ' , '; 
-    } 
-    //click event -button
-    onSave(event:any){    
-      console.log("Save button is clicked!"+ event.target);    
-    }    
-
-    val="";
-    handleInput1(element:any) {
-      this.val=element.value;
-    }
-}
-
-
-
-export class item {
-  code:number
-  itemName:string
-  
-  constructor(code:number,itemName:string) {
-    this.code=code;
-    this.itemName=itemName
   }
 
 }
