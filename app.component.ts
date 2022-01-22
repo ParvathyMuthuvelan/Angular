738bb16b1de8e6f8073d09aac8f6c1572fb8ad44
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2022-01-22 12:30:32 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -10,12 +10,20 @@ export class AppComponent {
   title = 'AngularProject';
   name= "Sai";
   clickCount=0;
-  
+  text="";
   isDisabled:boolean=true;
   isEnabled:boolean=false;
   cssStringVar: string= 'red size20';
   hasError:boolean =true;
   cssArray:string[]=['green','size20']; 
+  num:number =2;
+  showMe:boolean=false;
+  items= [
+    new item(1, 'Mobile'),
+    new item(2, 'Laptop'),
+    new item(3, 'Desktop'),
+    new item(4, 'Printer')
+  ]
 
   getTitle():string
   {
@@ -32,6 +40,38 @@ export class AppComponent {
 
   clickMe() {
     this.clickCount++;
+  }
+
+  show(event:any) {
+    console.log("Event target="+event.target);
+    console.log(event.target.value);
+  }  
+  
+  //keyup event
+  displayText(x:any) { 
+    console.log(x.target.value);
+  //this.text=this.text+x.target.value;
+      this.text += x.target.value + ' , '; 
+  } 
+
+    //click event -button
+    onSave(event:any){    
+      console.log("Save button is clicked!"+ event.target);    
+    }    
+
+    val="";
+    handleInput1(element:any) {
+      this.val=element.value;
+    }
+}
+
+export class item {
+  code:number
+  itemName:string
+  
+  constructor(code:number,itemName:string) {
+    this.code=code;
+    this.itemName=itemName
   }
 
 }
