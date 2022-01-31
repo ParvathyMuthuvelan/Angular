0dacbb6139f26b23ed5b41ce36c833b924c833ba
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2022-01-31 09:50:21 +0530

    Add files via upload

--- employee.service.ts
+++ employee.service.ts
@@ -1,46 +1,63 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpClientModule}   from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Employee } from './employee';
-
+import {HttpClient, HttpClientModule}   from '@angular/common/http';
+import { User } from './user';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
- 
- 
-  private url:string="/assets/data/employees.json";
-
- // employee: Employee=new Employee();
- employee={  
-    "id": 1,  
-    "name": "Jack",  
-    "email": "jack@gmail.com",  
-    "gender": "male"  
+  result: boolean = false;
+  private baseUrl = 'http://localhost:8080/api/v1';
+  validateUser(user: User): boolean {
+    
+   if((user.username=="admin") && (user.password=='admin@123') )
+   this.result=true;
+     return this.result;
   }
-    constructor(private http: HttpClient) {
-      
-     } // injecting http object into service class
-     fetchEmployees(): Observable<any> {
-      return this.http.get(this.url);
-    }
-    deleteEmployee(id:number):void
-    {
-   //to send http delete request
-    }
-    getEmployee(id:number):Employee
-    {
-   //to send http get request
+
+ 
+
+  constructor(private http: HttpClient) { }
+
+  validateEmployee(user: Object): Observable<Object> {
+    //alert("in service"+user);
+    return this.http.post(`${this.baseUrl}/loginUser`, user);
+    
+  }
+ 
+  getEmployees(): Observable<any> {
+    //console.log(this.http.get(`${this.baseUrl}`+'/getAllEmployees'));
+    return this.http.get(`${this.baseUrl}`+'/getAllEmployees');
+  }
+  getEmployee(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/getEmployee/${id}`);
+  }
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`+'/saveEmployee', employee);
+  }
+
+  updateEmployee(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/updateEmployee/${id}`, value);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/deleteEmployee/${id}`, { responseType: 'text' });
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem("username")
+    if (user === null) return false
+    return true
+}
+logout() {
+  sessionStorage.removeItem("username");
+  sessionStorage.setItem("login","failed");
   
-   return this.employee;
-    }
-    createEmployee(employee: Employee):void {
-     // return this.http.post(`${this.baseUrl}`+'/saveEmployee', employee);
-     console.log(employee);
-    }
-    updateEmployee(id: number, employee: Employee) {
-    //to send put request 
-    console.log(employee);
-    }
+}
+getLoginStatus()
+{
+  const loginStatus=sessionStorage.getItem("login");
+  return loginStatus;
+}
 }
